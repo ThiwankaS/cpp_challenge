7a7b2703a741557b09dbb2196f3d02843464875a
Thiwanka Somachandra <thivankas@outlook.com>
2025-11-02 03:46:22 +0200

    largest prime up to a given limit

---
+++ c004.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+bool isPrime(int n) {
+    if(n <= 3) {
+        return (n > 1);
+    } else if ( n % 2 == 0 || n % 3 == 0) {
+        return (false);
+    } else {
+        for(int i = 5; i * i <= n;i += 6) {
+            if(n % i == 0 || n % (i + 2) == 0) {
+                return (false);
+            }
+        }
+    }
+    return (true);
+}
+
+int main(void) {
+    int limit;
+    std::cout << "Upper limit : ";
+    std::cin >> limit;
+    for(int i = limit; i > 1; i--){
+        if(isPrime(i)) {
+            std::cout <<" large prime number : " << i << "\n";
+            return (EXIT_SUCCESS);
+        }
+    }
+    return (EXIT_SUCCESS);
+}
